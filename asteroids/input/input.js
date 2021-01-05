232bce3912ff305749de1f4d323d3ffe77662099
jollyr0ger <kav993@gmail.com>
2021-01-05 13:38:39 +0200

    KAV | minor fixes

--- asteroids/input/input.js
+++ asteroids/input/input.js
@@ -1,18 +1,18 @@
-import {inputCfg} from './inputCfg.js';
+import {shipControll} from './shipControll.js';
 
 export default function createInput(playerController){
   document.addEventListener('keydown', keyDown)
   document.addEventListener('keyup', keyUp)
 
   function keyDown(ev){
-    if(ev.key in inputCfg){
-      playerController.dispatch(inputCfg[ev.key] + '_on')
+    if(ev.key in shipControll){
+      playerController.dispatch(shipControll[ev.key] + '_on')
     }
   }
 
   function keyUp(ev){
-    if(ev.key in inputCfg){
-      playerController.dispatch(inputCfg[ev.key] + '_off')
+    if(ev.key in shipControll){
+      playerController.dispatch(shipControll[ev.key] + '_off')
     }
   }
 }
--- asteroids/input/shipControll.js
+++ asteroids/input/shipControll.js
@@ -1,4 +1,4 @@
-export const inputCfg = {
+export const shipControll = {
   'ArrowLeft': 'rotateLeft',
   'ArrowUp': 'thrust',
   'ArrowRight': 'rotateRight',
